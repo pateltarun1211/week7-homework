5b15db5efafe0ada3205fcceca7c9ce841e0f69c
pateltarun1211 <pateltarun1211@gmail.com>
2022-11-23 22:01:47 -0600

    forgot code wars

--- Day1_Homework/main.js
+++ Day1_Homework/main.js
@@ -44,3 +44,61 @@ replaceEvens(arr1)
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
+
+// ==== Codewars - 1 ====
+// ==== You're a square - 7kyu ====
+
+
+// A square of squares
+// You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
+
+// However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+// 25  =>  true
+// 26  =>  false
+// isSquare (-1) // => false
+// isSquare   3  // => false
+// isSquare   4  // => true
+// isSquare  25  // => true
+// isSquare  26  // => false
+
+
+var isSquare = function(n){
+    return Math.sqrt(n) % 1 === 0;
+    }
+
+
+
+// ==== Codewars - 2 ====
+// ==== Descending Order - 7kyu ====
+
+// Your task is to make a function that can take any non-negative integer as an argument
+// and return it with its digits in descending order.
+// Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n){
+    let n2 = n.toString().split('').sort(function (a,b) {return b-a;}).join('')
+    return n2
+  }
+
+console.log(descendingOrder(42145))
